7f6504d0c3af4c5243d347557c8504428f984ff2
satbirsoni <satbirsoni@gmail.com>
2016-09-20 17:10:39 +1000

    Added more templates for user interface controls

--- displayengine/displayengine.py
+++ displayengine/displayengine.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import sys
+from gui.line import ALine
+from gui.triangle import Triangle
 import math
 import math, random
 from PySide.QtCore import *
@@ -21,31 +23,6 @@ class Control:
     def render(self):
         pass
 
-
-
-
-
-class Triangle:
-    def __init__(self, center_x, center_y, width):
-        self.centerX = center_x
-        self.centerY = center_y
-        self.width = width
-
-    def render(self):
-        glBegin(GL_TRIANGLES)
-        x1 = self.centerX
-        x2 = self.centerX - self.width / 2
-        x3 = self.centerX + self.width / 2
-        y1 = self.centerY + self.width / 2
-        y2 = self.centerY - self.width / 2
-        y3 = self.centerY - self.width / 2
-
-        glColor3f(100, 122, 0.3)
-
-        glVertex3f(x1, y1, 0);
-        glVertex3f(x2, y2, 0);
-        glVertex3f(x3, y3, 0);
-        glEnd()
 
 class GLWidget(QtOpenGL.QGLWidget):
     zRotationChanged = QtCore.Signal(int)
---
+++ gui/triangle.py
@@ -0,0 +1,34 @@
+import sys
+
+from PySide import QtGui
+
+try:
+    from OpenGL.GL import *
+except ImportError:
+    app = QtGui.QApplication(sys.argv)
+    QtGui.QMessageBox.critical(None, "OpenGL grabber",
+                               "PyOpenGL must be installed to run this example.")
+    sys.exit(1)
+
+
+class Triangle:
+    def __init__(self, center_x, center_y, width):
+        self.centerX = center_x
+        self.centerY = center_y
+        self.width = width
+
+    def render(self):
+        glBegin(GL_TRIANGLES)
+        x1 = self.centerX
+        x2 = self.centerX - self.width / 2
+        x3 = self.centerX + self.width / 2
+        y1 = self.centerY + self.width / 2
+        y2 = self.centerY - self.width / 2
+        y3 = self.centerY - self.width / 2
+
+        glColor3f(100, 122, 0.3)
+
+        glVertex3f(x1, y1, 0);
+        glVertex3f(x2, y2, 0);
+        glVertex3f(x3, y3, 0);
+        glEnd()
