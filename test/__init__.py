5a91dbd5384824d5536ca21831419d171b6ef4f0
satbirsoni <satbirsoni@gmail.com>
2016-09-24 08:23:33 +1000

    Adding unit test

---
+++ test/__init__.py
@@ -0,0 +1,20 @@
+import unittest
+from gui import *
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+        self
+        print "In setup"
+        pass
+
+    def tearDown(self):
+        print "In teardown"
+
+    def test_render_all_controls(self):
+
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
