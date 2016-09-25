5d116b1fcd5198ec4d131a736900cf2319ea3594
satbirsoni <satbirsoni@gmail.com>
2016-09-25 10:22:20 +1000

    Line rendering correctly at this stage

--- gui/line.py
+++ gui/line.py
@@ -12,29 +12,42 @@ except ImportError:
     sys.exit(1)
 
 
-class ALine(object, control.Control):
-    def __init__(self, x1, y1, x2, y2, width):
-        print "ALine.__init__"
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-        self.width = width
+class ALine(control.Control):
+    def __init__(self):
+        """
+
+        """
+        super(control.Control, self).__init__()
         self._controls = {}
 
-    def render(self, bounds):
+    def render(self):
+        """
+
+        :return:
+        """
         self.draw_line()
         for key, value in self._controls.items():
             value.render()
+        return True
 
-    def add(self, pos, control):
-        self._controls[pos] = control
+    def add(self, pos, c):
+        """
+
+        :param pos:
+        :param c:
+        :return:
+        """
+        self._controls[pos] = c
 
     def draw_line(self):
+        """
+
+        :return:
+        """
         glBegin(GL_LINES)
         glColor3f(0, 1, 0)
-        glVertex2f(self.x1, self.y1)
-        glVertex2f(self.x2, self.y2)
+        glVertex2f(self.bounds.left_top.x, self.bounds.left_top.y)
+        glVertex2f(self.bounds.right_bottom.x, self.bounds.right_bottom.y)
         glEnd()
 
 
@@ -42,33 +55,24 @@ if __name__ == '__main__':
     list = []
 
     app = QtGui.QApplication(sys.argv)
-    lineA = ALine(-7.9, 1, 7.9, 1, .03)
-    lineB = ALine(-7.9, 2, 7.9, 2, .03)
-    lineC = ALine(-7.9, 3, 7.9, 3, .03)
-    lineD = ALine(-7.9, 4, 7.9, 4, .03)
-    lineE = ALine(-7.9, 5, 7.9, 5, .03)
-    lineF = ALine(-7.9, 6, 7.9, 6, .03)
-    list.append(lineA)
-    list.append(lineB)
-    list.append(lineC)
-    list.append(lineD)
-    list.append(lineE)
-    list.append(lineF)
-
-
-    import bubble
     import start
+    import circle
 
-    for i in range(10):
-        t = start.StartControl(-7.9 + i + 0.5, 1, 0.2)
-        lineA.add(i, t)
+    for i in range(-5, 8):
+        bounds = control.Bounds(control.Point(-7.9, i), control.Point(8, i))
+        lineA = ALine()
+        lineA.set_bounds(bounds)
+        list.append(lineA)
+        s = start.StartControl()
+        c = circle.Circle()
+        sbounds = control.Bounds(control.Point(-7.9, i - 0.2), control.Point(-6.9, i + 0.2))
+        sbounds.print_me()
+        c.set_bounds(sbounds)
 
-    import bubble
-    import start
+        s.set_bounds(sbounds)
+        list.append(c)
+        list.append(s)
 
-    for i in range(10):
-        b = bubble.Bubble(-7.9 + i, 2, 0.1)
-        lineB.add(i, b)
 
     import testbed
 
