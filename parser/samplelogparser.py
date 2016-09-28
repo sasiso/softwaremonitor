a3b295eb0bac93cbbd3ea79ccb9724f269491f89
satbirsoni <satbirsoni@gmail.com>
2016-09-28 19:22:39 +1000

    Updated Parser

--- parser/samplelogparser.py
+++ parser/samplelogparser.py
@@ -6,3 +6,5 @@ Format: 21-12-2014 21:22:59.0000 Process(12).
 """
 
 meta_regex='^(\d\d{1,2}-\d\d{1,2}-\d\d{1,4}\s\d\d{1,2}:\d\d{1,2}:\d\d{1,2}.\d{1,6}\s[\w]+\(\d+\)'
+parse_date_time_from_meta='^(\d\d{1,2}-\d\d{1,2}-\d\d{1,4}\s\d\d{1,2}:\d\d{1,2}:\d\d{1,2}.\d{1,6}\s)'
+extract_treadid_from_meta='\((\d+)\)$'
